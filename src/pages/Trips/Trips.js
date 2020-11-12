95a27054489fd9aad078d4d1bf18852fc851f000
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-12 15:58:13 -0800

    added search box

--- src/pages/Trips/Trips.js
+++ src/pages/Trips/Trips.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Trips.css";
+import SearchIcon from '@material-ui/icons/Search';
 import Card from "../../card-components/TripCards/TripCard";
 
 function Trips() {
@@ -8,6 +9,10 @@ function Trips() {
             <div className="trips-header">
                 <h1>TRIPS</h1>
                 <button id="add-trip">+</button>
+                <form>
+                    <input type="text" placeholder="Search Trips"/>
+                    <button id="trip-search-submit"><SearchIcon/></button>
+                </form>
             </div>
             <div className="trip-cards-container">
                 <Card/>
