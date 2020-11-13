94ca91f411ab1629b89684b0cfbaedb73b7ba514
Chris Sison <chris.a.sison@outlook.com>
2020-11-13 15:45:05 -0800

    update

--- src/card-components/TripCards/TripCard.js
+++ src/card-components/TripCards/TripCard.js
@@ -17,6 +17,7 @@ import CheckIcon from '@material-ui/icons/Check';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -97,7 +98,7 @@ export default function TripCard(props) {
             { complete ?
             <div className="textBlue"><CheckIcon/></div>
             :
-            <div className="textGrey"><ClearIcon/></div>}
+            <div className="textGrey"><MoreHorizIcon/></div>}
         </IconButton>
       </CardActions>
     </Card>
