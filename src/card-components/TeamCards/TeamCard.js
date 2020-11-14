7b405a869de1397109366f265db6013727e7027b
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-13 17:21:23 -0800

    added scrollbar to teams cards

--- src/card-components/TeamCards/TeamCard.js
+++ src/card-components/TeamCards/TeamCard.js
@@ -9,7 +9,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
-import ClearIcon from '@material-ui/icons/Clear';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
@@ -160,7 +160,7 @@ export default function TeamCard(props) {
       <CardActions disableSpacing style={{position:'relative'}}>
         <IconButton onClick={switchActive} aria-label="add to favorites">
             { active ?
-            <div className="textBlue"><ClearIcon/></div>
+            <div className="textBlue"><MoreHorizIcon/></div>
             :
             <div className="textGrey"><CheckIcon/></div>}
         </IconButton>
--- src/card-components/TripCards/TripCard.js
+++ src/card-components/TripCards/TripCard.js
@@ -9,7 +9,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
-import ClearIcon from '@material-ui/icons/Clear';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
@@ -95,9 +95,9 @@ export default function TripCard(props) {
       <CardActions disableSpacing>
         <IconButton onClick={setComplete} aria-label="add to favorites">
             { complete ?
-            <div className="textBlue"><CheckIcon/></div>
+            <div className="textBlue"><MoreHorizIcon/></div>
             :
-            <div className="textGrey"><ClearIcon/></div>}
+            <div className="textGrey"><CheckIcon/></div>}
         </IconButton>
       </CardActions>
     </Card>
