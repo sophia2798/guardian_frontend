eaae77a4f4608c09ebd7cc9e4f471b849d4ff41e
Chris Sison <chris.a.sison@outlook.com>
2020-11-17 15:04:14 -0800

    whiteboard

--- src/pages/Dashboard/TextEditor.js
+++ src/pages/Dashboard/TextEditor.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./TextEditor.css";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 
 function TextEditor() {
-    return (
-        <div>
-            
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput("");
+  };
+  return (
+    <div className="textEditor">
+      <div className="textEditor__top">
+        <h4>Whiteboard</h4>
+        <form>
+          <textarea
+            value=""
+            onChange={(e) => setInput(e.target.value)}
+            className="textEditor__input"
+          />
+          <button onClick="" type="submit">
+            Hidden Submit
+          </button>
+        </form>
+      </div>
+
+      <div className="textEditor__bottom">
+        <div className="textEditor__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Upload Video</h3>
         </div>
-    )
+        <div className="textEditor__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Upload Image</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TextEditor
+export default TextEditor;
