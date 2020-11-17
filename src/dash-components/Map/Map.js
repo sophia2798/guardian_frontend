d3c8e509a456e323b5eaee9266767e6cbf789a59
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-16 22:36:10 -0800

    added pins from seed data itinerary, added custom pin icon

--- src/dash-components/Map/Map.js
+++ src/dash-components/Map/Map.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
-import { withGoogleMap, GoogleMap } from "react-google-maps";
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import "./Map.css";
+import Pin from "./pin2.png";
+import TripSeed from "../../utils/seedTrip.json";
 const styles = require("./GoogleMapStyles.json");
 
 class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itinerary: TripSeed[2].itinerary,
+      info: false
+    }
+  }
+
+  toggleInfo = () => {
+    this.setState({ info: !this.state.info })
+  }
+  
   render() {
     const GoogleMapExample = withGoogleMap((props) => (
       <GoogleMap
         defaultCenter={{ lat: 47.605, lng: -122.353 }}
         defaultZoom={13}
         defaultOptions={{ styles: styles }}
-      ></GoogleMap>
+      >
+        {this.state.itinerary.map(place => (
+          <div className="marker">
+          <Marker position={{lat: place.coordinates.lat, lng: place.coordinates.long}} title={place.location} icon={{url: Pin}} onClick={this.toggleInfo}/>
+          <div className="marker-info" style={this.state.info ? {display:'block'} : {display:'none'}}>sdfsdfsdf</div>
+          </div>
+        ))}
+
+      </GoogleMap>
     ));
+
     return (
       <div className="map__container">
         <GoogleMapExample
@@ -22,4 +45,5 @@ class Map extends Component {
     );
   }
 }
+
 export default Map;
