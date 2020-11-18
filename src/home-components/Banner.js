71fec08e23bfaa5fed88c9a3b178ed7c1239c98f
Chris Sison <chris.a.sison@outlook.com>
2020-11-18 09:22:03 -0800

    update banner

--- src/home-components/Banner.js
+++ src/home-components/Banner.js
@@ -12,43 +12,43 @@ function Banner() {
                     <h2 className="overlay__text">VALUES</h2>
                 </div>
                 <div className="banner__overlay2">
-                    <h3 className="overlay__text2">LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. ETIAM NON NIBH EU LECTUS SAGITTIS TINCIDUNT NEC AC NIBH. VESTIBULUM BIBENDUM, METUS AC EUISMOD ALIQUET, DUI JUSTO EFFICITUR ENIM, NEC PLACERAT LIBERO NIBH VITAE MASSA. QUISQUE PORTA UT AUGUE SIT AMET ACCUMSAN. MAECENAS HENDRERIT MAGNA VITAE MAURIS MOLESTIE PLACERAT. ETIAM ACCUMSAN AUCTOR SOLLICITUDIN. MAECENAS LIGULA NISL, VIVERRA QUIS AUCTOR IN, CONGUE EU LECTUS. QUISQUE EGET IACULIS DUI, ID COMMODO TELLUS.</h3>
+                    <h3 className="overlay__text2">Integrity. Quality. Partnership.</h3>
                 </div>
             </div>
             <div className="banner_img_wrapper">
                 <img className="banner__image" src="https://images.pexels.com/photos/364096/pexels-photo-364096.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
                 <div className="banner__overlay">
-                    <h2 className="overlay__text">MOTIVATIONS</h2>
+                    <h2 className="overlay__text">SERVICES</h2>
                 </div>
                 <div className="banner__overlay2">
-                    <h3 className="overlay__text2">LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. ETIAM NON NIBH EU LECTUS SAGITTIS TINCIDUNT NEC AC NIBH. VESTIBULUM BIBENDUM, METUS AC EUISMOD ALIQUET, DUI JUSTO EFFICITUR ENIM, NEC PLACERAT LIBERO NIBH VITAE MASSA. QUISQUE PORTA UT AUGUE SIT AMET ACCUMSAN. MAECENAS HENDRERIT MAGNA VITAE MAURIS MOLESTIE PLACERAT. ETIAM ACCUMSAN AUCTOR SOLLICITUDIN. MAECENAS LIGULA NISL, VIVERRA QUIS AUCTOR IN, CONGUE EU LECTUS. QUISQUE EGET IACULIS DUI, ID COMMODO TELLUS.</h3>
+                    <h3 className="overlay__text2">Application Development. Information and Data Security. Risk Assessment. Data Analysis.</h3>
                 </div>
             </div>
             <div className="banner_img_wrapper">
                 <img className="banner__image" src="https://images.pexels.com/photos/2411688/pexels-photo-2411688.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
                 <div className="banner__overlay">
-                    <h2 className="overlay__text">STORIES</h2>
+                    <h2 className="overlay__text">APPROACH</h2>
                 </div>
                 <div className="banner__overlay2">
-                    <h3 className="overlay__text2">LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. ETIAM NON NIBH EU LECTUS SAGITTIS TINCIDUNT NEC AC NIBH. VESTIBULUM BIBENDUM, METUS AC EUISMOD ALIQUET, DUI JUSTO EFFICITUR ENIM, NEC PLACERAT LIBERO NIBH VITAE MASSA. QUISQUE PORTA UT AUGUE SIT AMET ACCUMSAN. MAECENAS HENDRERIT MAGNA VITAE MAURIS MOLESTIE PLACERAT. ETIAM ACCUMSAN AUCTOR SOLLICITUDIN. MAECENAS LIGULA NISL, VIVERRA QUIS AUCTOR IN, CONGUE EU LECTUS. QUISQUE EGET IACULIS DUI, ID COMMODO TELLUS.</h3>
+                    <h3 className="overlay__text2">Our approach allows clients to be successful in planning and implementing their protection strategy while integrating modern web and mobile applications with the highest levels of data security.</h3>
                 </div>
             </div>
             <div className="banner_img_wrapper">
                 <img className="banner__image" src="https://images.pexels.com/photos/1563256/pexels-photo-1563256.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
                 <div className="banner__overlay">
-                    <h2 className="overlay__text">TEAMS</h2>
+                    <h2 className="overlay__text">TECHNOLOGIES</h2>
                 </div>
                 <div className="banner__overlay2">
-                    <h3 className="overlay__text2">LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. ETIAM NON NIBH EU LECTUS SAGITTIS TINCIDUNT NEC AC NIBH. VESTIBULUM BIBENDUM, METUS AC EUISMOD ALIQUET, DUI JUSTO EFFICITUR ENIM, NEC PLACERAT LIBERO NIBH VITAE MASSA. QUISQUE PORTA UT AUGUE SIT AMET ACCUMSAN. MAECENAS HENDRERIT MAGNA VITAE MAURIS MOLESTIE PLACERAT. ETIAM ACCUMSAN AUCTOR SOLLICITUDIN. MAECENAS LIGULA NISL, VIVERRA QUIS AUCTOR IN, CONGUE EU LECTUS. QUISQUE EGET IACULIS DUI, ID COMMODO TELLUS.</h3>
+                    <h3 className="overlay__text2">Loading...</h3>
                 </div>
             </div>
             <div className="banner_img_wrapper">
                 <img className="banner__image" src="https://images.pexels.com/photos/1755683/pexels-photo-1755683.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
                 <div className="banner__overlay">
-                    <h2 className="overlay__text">FUTURE</h2>
+                    <h2 className="overlay__text">THE TEAM</h2>
                 </div>
                 <div className="banner__overlay2">
-                    <h3 className="overlay__text2">LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. ETIAM NON NIBH EU LECTUS SAGITTIS TINCIDUNT NEC AC NIBH. VESTIBULUM BIBENDUM, METUS AC EUISMOD ALIQUET, DUI JUSTO EFFICITUR ENIM, NEC PLACERAT LIBERO NIBH VITAE MASSA. QUISQUE PORTA UT AUGUE SIT AMET ACCUMSAN. MAECENAS HENDRERIT MAGNA VITAE MAURIS MOLESTIE PLACERAT. ETIAM ACCUMSAN AUCTOR SOLLICITUDIN. MAECENAS LIGULA NISL, VIVERRA QUIS AUCTOR IN, CONGUE EU LECTUS. QUISQUE EGET IACULIS DUI, ID COMMODO TELLUS.</h3>
+                    <h3 className="overlay__text2">Chris MacGeorge. Sophia Jung. Chris Sison. Jack Solaro. Katie Dickson. Nick VanBaak.</h3>
                 </div>
             </div>
         </div>
