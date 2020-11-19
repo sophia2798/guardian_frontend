e04fafd184b0ffc8f74baf29dfbcd7917fa46180
jacksolaro <jacksolaro@gmail.com>
2020-11-19 12:36:11 -0800

    added sign up api request

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,30 +1,47 @@
 const API_PREFIX = "http://localhost:9164/api";
 
-const API =  {
-    login: function(userData) {
-        return fetch(`${API_PREFIX}/users/signin`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userData)
-        }).then(res => res.json()).catch(err => null)
-    },
-    getProfile: function(token) {
-        return fetch(`${API_PREFIX}/userInfo`, {
-            headers: {
-                'authorization': `Bearer ${token}`
-            }
-        }).then(res => res.json()).catch(err => null)
-    },
-    deleteTrip: function(token, TripID) {
-        return fetch(`${API_PREFIX}/trips/${TripID}`, {
-            method: 'DELETE',
-            headers: {
-                'authorization': `Bearer ${token}`
-            }
-        }).then(res => res.json()).catch(err => null)
-    }
-}
+const API = {
+  login: function (userData) {
+    return fetch(`${API_PREFIX}/users/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((res) => res.json())
+      .catch((err) => null);
+  },
+  signup: function (userData) {
+    return fetch(`${API_PREFIX}/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((res) => res.json())
+      .catch((err) => null);
+  },
+  getProfile: function (token) {
+    return fetch(`${API_PREFIX}/userInfo`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .catch((err) => null);
+  },
+  deleteTrip: function (token, TripID) {
+    return fetch(`${API_PREFIX}/trips/${TripID}`, {
+      method: "DELETE",
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .catch((err) => null);
+  },
+};
 
 module.exports = API;
