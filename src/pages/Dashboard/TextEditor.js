d3944d8efd99eba7f17895c27c3216cee8e6073e
jacksolaro <jacksolaro@gmail.com>
2020-11-20 11:42:04 -0800

    connected text editor to back end

--- src/pages/Dashboard/TextEditor.js
+++ src/pages/Dashboard/TextEditor.js
@@ -2,11 +2,28 @@ import React, { useState } from "react";
 import "./TextEditor.css";
 import VideocamIcon from "@material-ui/icons/Videocam";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import API from "../../utils/API";
 
-function TextEditor() {
+function TextEditor(props) {
+  const [reportDoc, setReportDoc] = React.useState({
+    report_doc: props.data,
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    API.updateWhiteboard(props.token, props.trip, reportDoc).then(() => {
+      console.log("successfully saved", reportDoc);
+    });
   };
+
+  const handleInputChange = (event) => {
+    setReportDoc({ report_doc: event.target.value });
+  };
+
+  React.useEffect(() => {
+    setReportDoc({ report_doc: props.data });
+  }, [props.data]);
+
   return (
     <div className="textEditor">
       <div className="textEditor__top">
@@ -14,10 +31,15 @@ function TextEditor() {
         <form>
           <textarea
             className="textEditor__input"
+            defaultValue={reportDoc.report_doc}
+            onChange={handleInputChange}
           />
-          <button onClick="" type="submit">
-            Hidden Submit
-          </button>
+          <input
+            className="login-submit-btn"
+            value="SAVE"
+            type="submit"
+            onClick={(e) => handleSubmit(e)}
+          />
         </form>
       </div>
 
